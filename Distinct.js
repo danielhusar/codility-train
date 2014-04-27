b3a41c46b880fc0b3d74a4db73293987c4265189
Daniel Husar <dano.husar@gmail.com>
2014-04-27 22:06:07 +0100

    Create Distinct.js

---
+++ Distinct.js
@@ -0,0 +1,23 @@
+function solution(A) {
+    var distinct = {};
+    var count = 0;
+    
+    for (var i=0; i<A.length; i++){
+        if (! distinct[A[i]]) {
+            distinct[A[i]] = true;
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+/**
+
+function solution(A) {
+    return A.filter(function (e, i) {
+        return A.lastIndexOf(e) === i;
+    }).length;
+}
+
+**/
