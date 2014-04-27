240b027cefe378b681070bcb63e1cff7945b4398
Daniel Husar <dano.husar@gmail.com>
2014-04-27 22:37:08 +0100

    Create MaxProductOfThree.js

---
+++ MaxProductOfThree.js
@@ -0,0 +1,9 @@
+function solution(A) {
+    A.sort(function(a,b){return b-a;});
+    return Math.max(
+        A[0] * A[1] * A[2],
+        A[0] * A[1] * A[A.length-1],
+        A[0] * A[A.length-1] * A[A.length-2],
+        A[A.length-1] * A[A.length-2] * A[A.length-3]
+    );
+}
